256d84a44ecf989e7a28eab4c342a119474cd479
turbojax07 <turbojax07@gmail.com>
2025-11-27 19:14:43 -0500

    adjusting the constructor for the gui with the list of recipes

--- src/main/java/com/catadmirer/infuseSMP/inventories/RecipeListGUI.java
+++ src/main/java/com/catadmirer/infuseSMP/inventories/RecipeListGUI.java
@@ -1,6 +1,5 @@
 package com.catadmirer.infuseSMP.inventories;
 
-import java.util.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
@@ -12,20 +11,15 @@ import net.kyori.adventure.text.Component;
 public class RecipeListGUI implements InventoryHolder {
     private final Inventory inventory;
 
-    public RecipeListGUI(Map<String, Map<String, Integer>> craftLimits) {
+    public RecipeListGUI() {
         inventory = Bukkit.createInventory(null, 36, Component.text("Potion Crafting"));
 
         int[] customSlots = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33};
         int index = 0;
-        for (String potionName : craftLimits.keySet()) {
+        for (String potionName : Recipes.recipeKeys) {
             if (index >= customSlots.length) break;
-
-            Map<String, Integer> limits = craftLimits.get(potionName);
-            int augmentedLimit = limits.get("augmented_limit");
-            int regularLimit = limits.get("regular_limit");
-            ItemStack potion = Recipes.createPotionWithModifiedLore(potionName, augmentedLimit, regularLimit);
+            ItemStack potion = Recipes.createPotionWithModifiedLore(potionName);
             inventory.setItem(customSlots[index] - 1, potion);
-
             index++;
         }
     }
