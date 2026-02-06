636baeca098e14fe9b5ddd4c11c4013b321730da
TurboJax07 <turbojax07@gmail.com>
2026-02-06 10:56:31 -0500

    Refactoring Drop.java

--- src/main/java/com/catadmirer/infuseSMP/managers/Drop.java
+++ src/main/java/com/catadmirer/infuseSMP/managers/Drop.java
@@ -8,11 +8,10 @@ import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
@@ -24,38 +23,28 @@ public class Drop implements Listener {
         this.plugin = plugin;
     }
 
-    private boolean isPotion(ItemStack item) {
+    private boolean isInfuseEffect(ItemStack item) {
         return item != null && item.getType() == Material.POTION && item.getItemMeta().hasCustomModelData();
     }
 
     public void onPickup(EntityPickupItemEvent event) {
-        // Making sure the entity is a player
-        if (!(event.getEntity() instanceof Player player)) return;
-
-        // Making sure the item has been picked up.
-        if (event.isCancelled()) return;
-
         ItemStack item = event.getItem().getItemStack();
-        if (this.isPotion(item)) {
-            this.playDustEffect(player, true, EffectMapping.fromItem(item), event.getItem().getLocation());
+        if (this.isInfuseEffect(item)) {
+            this.playDustEffect(true, EffectMapping.fromItem(item), event.getItem().getLocation());
         }
     }
 
     @EventHandler
-    public void onDrop(PlayerDropItemEvent event) {
+    public void onDrop(EntityDropItemEvent event) {
         final Item droppedItem = event.getItemDrop();
         ItemStack itemStack = droppedItem.getItemStack();
-        if (this.isPotion(itemStack)) {
-            this.playDustEffectDrop(event.getPlayer(), false, EffectMapping.fromItem(itemStack), droppedItem.getLocation());
-            (new BukkitRunnable() {
-                public void run() {
-                        droppedItem.setGlowing(true);
-                }
-            }).runTaskLater(this.plugin, 1L);
+        if (this.isInfuseEffect(itemStack)) {
+            this.playDustEffectDrop(false, EffectMapping.fromItem(itemStack), droppedItem.getLocation());
+            droppedItem.setGlowing(true);
         }
     }
 
-    private void playDustEffect(Player player, final boolean bottomToTop, @NotNull EffectMapping effect, Location location) {
+    private void playDustEffect(final boolean bottomToTop, @NotNull EffectMapping effect, Location location) {
         final Location base = location.add(0, 0.1, 0);
         final World world = location.getWorld();
         Color color = Color.fromRGB(effect.getColor().getRGB());
@@ -85,7 +74,7 @@ public class Drop implements Listener {
         world.playSound(base, Sound.ENTITY_TURTLE_EGG_BREAK, 1.3F, 1.2F);
     }
 
-    private void playDustEffectDrop(Player player, final boolean bottomToTop, EffectMapping effect, Location location) {
+    private void playDustEffectDrop(final boolean bottomToTop, EffectMapping effect, Location location) {
         final Location base = location.add(0, -1.5, 0);
         final World world = location.getWorld();
         Color color = Color.fromRGB(effect.getColor().getRGB());
