f683a9dea30f357e960ba40cdf829bb7d3ba6199
TurboJax <turbojax07@gmail.com>
2026-02-13 20:48:28 -0500

    Adding more checks to ItemUtil

--- src/main/java/com/catadmirer/infuseSMP/util/ItemUtil.java
+++ src/main/java/com/catadmirer/infuseSMP/util/ItemUtil.java
@@ -4,15 +4,33 @@ import org.bukkit.inventory.ItemStack;
 import com.destroystokyo.paper.MaterialSetTag;
 
 public class ItemUtil {
+    public static boolean isSword(ItemStack item) {
+        if (item == null) return false;
+
+        return MaterialSetTag.ITEMS_SWORDS.isTagged(item.getType());
+    }
+
+    public static boolean isPickaxe(ItemStack item) {
+        if (item == null) return false;
+
+        return MaterialSetTag.ITEMS_PICKAXES.isTagged(item.getType());
+    }
+
     public static boolean isAxe(ItemStack item) {
         if (item == null) return false;
 
         return MaterialSetTag.ITEMS_AXES.isTagged(item.getType());
     }
 
-    public static boolean isSword(ItemStack item) {
+    public static boolean isShovel(ItemStack item) {
         if (item == null) return false;
 
-        return MaterialSetTag.ITEMS_SWORDS.isTagged(item.getType());
+        return MaterialSetTag.ITEMS_SHOVELS.isTagged(item.getType());
+    }
+
+    public static boolean isHoe(ItemStack item) {
+        if (item == null) return false;
+
+        return MaterialSetTag.ITEMS_HOES.isTagged(item.getType());
     }
 }
