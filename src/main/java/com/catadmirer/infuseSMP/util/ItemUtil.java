d3355c907e9c0755317934d1def6dc5bfe7956ac
TurboJax <turbojax07@gmail.com>
2026-02-14 03:15:36 -0500

    Adding a new function to ItemUtil

--- src/main/java/com/catadmirer/infuseSMP/util/ItemUtil.java
+++ src/main/java/com/catadmirer/infuseSMP/util/ItemUtil.java
@@ -1,5 +1,6 @@
 package com.catadmirer.infuseSMP.util;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import com.destroystokyo.paper.MaterialSetTag;
 
@@ -32,5 +33,9 @@ public class ItemUtil {
         if (item == null) return false;
 
         return MaterialSetTag.ITEMS_HOES.isTagged(item.getType());
+    }
+
+    public static void giveOrDropItem(Player player, ItemStack... items) {
+        player.getInventory().addItem(items).forEach((i, extra) -> player.getWorld().dropItem(player.getLocation(), extra));
     }
 }
