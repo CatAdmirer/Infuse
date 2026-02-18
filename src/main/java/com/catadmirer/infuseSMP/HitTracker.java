eb3842314de6648b3da2dfcb1101ae12fdfd41d1
TurboJax <turbojax07@gmail.com>
2026-02-18 01:11:16 -0500

    Fixing the HitTracker to make hits decay over time and actually calling TenHitEvent on the tenth hit

--- src/main/java/com/catadmirer/infuseSMP/HitTracker.java
+++ src/main/java/com/catadmirer/infuseSMP/HitTracker.java
@@ -1,19 +1,22 @@
 package com.catadmirer.infuseSMP;
 
+import com.catadmirer.infuseSMP.events.TenHitEvent;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
 import java.util.UUID;
-
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import com.catadmirer.infuseSMP.events.TenHitEvent;
-
-public class HitTracker {
+public class HitTracker implements Listener {
     private final Infuse plugin;
     private final Map<UUID,Integer> hitTracker = new HashMap<>();
+    Queue<Runnable> decayQueue = new ConcurrentLinkedQueue<>();
 
     public HitTracker(Infuse plugin) {
         this.plugin = plugin;
@@ -24,32 +27,68 @@ public class HitTracker {
      * 
      * @param event A {@link EntityDamageByEntityEvent}
      */
+    @EventHandler
     public void onPlayerHit(EntityDamageByEntityEvent event) {
         // Making sure both entities are players
         if (!(event.getDamager() instanceof Player attacker)) return;
         if (!(event.getEntity() instanceof Player target)) return;
 
+        Infuse.debug(attacker.getName() + " has hit " + target.getName());
+
         // Making sure it counts as a normal hit
         // Vanilla attack cooldown needs to be at 84.8% to be a normal hit.
-        if (attacker.getAttackCooldown() < 0.85) return;
+        if (attacker.getAttackCooldown() < 0.85) {
+            Infuse.debug("Hit ignored due to being under attack cooldown threshold.");
+            return;
+        }
 
         // Incrementing the hit counter
-        int hits = hitTracker.getOrDefault(attacker, 0) + 1;
-        hitTracker.put(attacker.getUniqueId(), hits);
+        int hits = hitTracker.getOrDefault(attacker.getUniqueId(), 0) + 1;
+        Infuse.debug(attacker.getName() + "'s hit counter is " + hits + ".");
 
-        // Calling the TenHitEvent
-        TenHitEvent tenHit = new TenHitEvent(attacker, target);
-        tenHit.callEvent();
+        if (hits == 10) {
+            // Calling the TenHitEvent
+            TenHitEvent tenHit = new TenHitEvent(attacker, target);
+            tenHit.callEvent();
+            Infuse.debug("Called TenHitEvent");
+
+            hits -= 10;
+
+            // Removing 10 objects from the queue
+            for (int i = 0; i < 10; i++) {
+                if (decayQueue.isEmpty()) continue;
+                decayQueue.remove();
+            }
+            Infuse.debug("Removed items from queue.");
+        }
+
+        // Saving the hit count
+        hitTracker.put(attacker.getUniqueId(), hits);
 
         // Having the hit counter decay over time
         // TODO: make this a config (0 or below disables it)
         int hitCounterDecaySeconds = 10;
         if (hitCounterDecaySeconds < 1) return;
 
-        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+        Infuse.debug("Adding item to decay queue");
+        decayQueue.add(() -> {
+            // Skipping if the attacker has left the game
             if (!attacker.isConnected()) return;
 
-            hitTracker.put(attacker.getUniqueId(), hits - 1);
+            Infuse.debug("Decrementing hit counter");
+            int curHits = hitTracker.get(attacker.getUniqueId());
+
+            Infuse.debug(attacker.getName() + "'s hit counter is " + (curHits - 1) + ".");
+            hitTracker.put(attacker.getUniqueId(), curHits - 1);
+        });
+        
+        // Running the decay task if it is still around
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            Runnable decayTask = decayQueue.peek();
+            if (decayTask != null) {
+                decayQueue.remove();
+                decayTask.run();
+            }
         }, hitCounterDecaySeconds * 20);
     }
 
