b32927ce0b67bc23710b3f8dfa4c5ed0c2ad493e
CatAdmirer <catadmirerrr@gmail.com>
2026-02-19 12:09:04 +0000

    Just spark left on regen

--- src/main/java/com/catadmirer/infuseSMP/effects/Regen.java
+++ src/main/java/com/catadmirer/infuseSMP/effects/Regen.java
@@ -1,18 +1,34 @@
 package com.catadmirer.infuseSMP.effects;
 
 import com.catadmirer.infuseSMP.Infuse;
+import com.catadmirer.infuseSMP.InfuseDebug;
+import com.catadmirer.infuseSMP.events.TenHitEvent;
 import com.catadmirer.infuseSMP.managers.CooldownManager;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+
+import com.destroystokyo.paper.MaterialSetTag;
+import io.papermc.paper.datacomponent.item.FoodProperties;
 import org.bukkit.Bukkit;
 import com.catadmirer.infuseSMP.managers.EffectMapping;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.components.FoodComponent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Regen implements Listener {
     private static Infuse plugin;
@@ -21,33 +37,68 @@ public class Regen implements Listener {
         Regen.plugin = plugin;
     }
 
+    public static void applyPassiveEffects(Player player) {
+        if (plugin.getDataManager().hasEffect(player, EffectMapping.REGEN) ) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 30, 0, false, false));
+        }
+    }
+
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player player) {
             if (plugin.getDataManager().hasEffect(player, EffectMapping.REGEN)) {
-                player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20, 1, false, false));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 1, false, false));
             }
         }
     }
 
     @EventHandler
-    public void onEntityDamageByEntityHeal(EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof Player player) {
+    public void consume(PlayerItemConsumeEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.getDataManager().hasEffect(player, EffectMapping.REGEN)) {
+            float sat = player.getSaturation();
+            player.setSaturation(sat + 6);
+        }
+    }
+
+    @EventHandler
+    public void rclickfood(PlayerInteractEvent event) {
+        if (!(event.getAction().isRightClick())) return;
+        Player player = event.getPlayer();
+        if (event.getItem().getType().isEdible()) {
             if (plugin.getDataManager().hasEffect(player, EffectMapping.REGEN)) {
-                if (event.getFinalDamage() <= 0) {
-                    player.setSaturation(Math.min(player.getSaturation() + 6, 20));
-                }
+                event.getItem().editMeta(meta -> {
+                    meta.getFood().setCanAlwaysEat(true);
+                });
+            } else {
+                event.getItem().editMeta(meta -> {
+                    meta.getFood().setCanAlwaysEat(false);
+                });
             }
         }
     }
 
     @EventHandler
-    public void onEntityDamageByPlayer(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player damager) {
-            if (CooldownManager.isEffectActive(damager.getUniqueId(), "regen")) {
-                double damage = event.getFinalDamage();
-                damager.setHealth(Math.min(damager.getHealth() + damage / 2, damager.getAttribute(Attribute.MAX_HEALTH).getValue()));
+    public void onTenthAttack(TenHitEvent event) {
+        if (!plugin.getDataManager().hasEffect(event.getAttacker(), EffectMapping.REGEN)) return;
+
+        int currentFood = event.getTarget().getFoodLevel();
+        event.getTarget().setFoodLevel(currentFood - 2);
+    }
+
+    public Map<Player,Integer> foodLevelMap = new HashMap<>();
+
+    @EventHandler
+    public void foodChange(FoodLevelChangeEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+        if (!plugin.getDataManager().hasEffect(player, EffectMapping.REGEN)) return;
+
+        if (foodLevelMap.containsKey(player)) {
+            if (event.getFoodLevel() < foodLevelMap.get(player)) {
+                event.setFoodLevel(foodLevelMap.get(player));
             }
+        } else {
+            foodLevelMap.put(player, event.getFoodLevel());
         }
     }
 
