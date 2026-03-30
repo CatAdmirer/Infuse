bb2bbb2ea86297385bf446185cd9dea98b393274
TurboJax <turbojax07@gmail.com>
2026-03-30 05:18:30 -0400

    Centering effects on the action bar

--- src/main/java/com/catadmirer/infuseSMP/managers/ActionBarUpdater.java
+++ src/main/java/com/catadmirer/infuseSMP/managers/ActionBarUpdater.java
@@ -1,16 +1,15 @@
 package com.catadmirer.infuseSMP.managers;
 
 import com.catadmirer.infuseSMP.Infuse;
-import com.catadmirer.infuseSMP.Message;
 import com.catadmirer.infuseSMP.util.MessageUtil;
 import java.util.UUID;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class ActionBarUpdater extends BukkitRunnable {
+    private final MiniMessage mm = MiniMessage.miniMessage();
     private final Infuse plugin;
 
     public ActionBarUpdater(Infuse plugin) {
@@ -23,73 +22,133 @@ public class ActionBarUpdater extends BukkitRunnable {
             UUID uuid = player.getUniqueId();
 
             // Composing the action bar
-            Component actionBar = getPart1(uuid, "1")
-                    .append(Component.space())
-                    .append(getPart2(uuid, "2"));
+            String key;
+            EffectMapping effect;
+
+            String placeholder = plugin.getMainConfig().emptyEffectIcon() ? "\ue901\ue904" : "";
+
+            String leftPad = "";
+            String leftTime = "";
+            String leftEmoji = placeholder;
+            String rightEmoji = placeholder;
+            String rightTime = "";
+            String rightPad = "";
+
+            // Loading info for the first effect
+            effect = plugin.getDataManager().getEffect(uuid, "1");
+            if (effect != null) {
+                leftEmoji = effect.getIcon() + "\ue904";
+
+                key = effect.regular().getKey();
+                if (CooldownManager.isEffectActive(uuid, key)) {
+                    leftEmoji = String.valueOf(effect.getActiveIcon());
+
+                    long timeLeft = CooldownManager.getEffectTimeLeft(uuid, key) / 1000L;
+                    leftTime = "<#" + Integer.toHexString(effect.getColor().getRGB() & 0xFFFFFF) + ">" + MessageUtil.formatTime(timeLeft);
+                    rightPad = getSpaceTimeStr(mm.stripTags(leftTime));
+                } else if (CooldownManager.isOnCooldown(uuid, key)) {
+                    long timeLeft = CooldownManager.getCooldownTimeLeft(uuid, key) / 1000L;
+                    leftTime = MessageUtil.formatTime(timeLeft);
+                    rightPad = getSpaceTimeStr(mm.stripTags(leftTime));
+                }
+            }
+
+            // Loading info for the second effect
+            effect = plugin.getDataManager().getEffect(uuid, "2");
+            if (effect != null) {
+                rightEmoji = effect.getIcon() + "\ue904";
+
+                key = effect.regular().getKey();
+                if (CooldownManager.isEffectActive(uuid, key)) {
+                    rightEmoji = String.valueOf(effect.getActiveIcon());
+
+                    long timeLeft = CooldownManager.getEffectTimeLeft(uuid, key) / 1000L;
+                    rightTime = "<#" + Integer.toHexString(effect.getColor().getRGB() & 0xFFFFFF) + ">" + MessageUtil.formatTime(timeLeft);
+                    leftPad = getSpaceTimeStr(mm.stripTags(rightTime));
+                } else if (CooldownManager.isOnCooldown(uuid, key)) {
+                    long timeLeft = CooldownManager.getCooldownTimeLeft(uuid, key) / 1000L;
+                    rightTime = MessageUtil.formatTime(timeLeft);
+                    leftPad = getSpaceTimeStr(mm.stripTags(rightTime));
+                }
+            }
+
+            String fmt = String.format("<b>%s%s</b> <white>%s %s <b>%s%s</b>", leftPad, leftTime, leftEmoji, rightEmoji, rightTime, rightPad);
+
+            Component msg = mm.deserialize(fmt);
 
             // Sending the action bar
-            if (!actionBar.equals(Component.empty())) {
-                player.sendActionBar(actionBar);
-            }
+            player.sendActionBar(msg);
         });
     }
 
-    private Component getPart1(UUID uuid, String slot) {
-        Component time = Component.empty();
-
-        // Getting the effect equipped in the slot
-        EffectMapping effect = plugin.getDataManager().getEffect(uuid, slot);
-
-        // Handling empty slots
-        boolean emptyEffectEmoji = plugin.getMainConfig().emptyEffectIcon();
-        if (effect == null) {
-            return emptyEffectEmoji ? Component.text("\uE901 ") : Component.empty();
-        }
-
-        // Getting the right emoji to use and time to display
-        String key = effect.regular().getKey();
-        char emoji = effect.getIcon();
-        if (CooldownManager.isEffectActive(uuid, key)) {
-            long timeLeft = CooldownManager.getEffectTimeLeft(uuid, key) / 1000L;
-            time = MessageUtil.formatTime(timeLeft, TextColor.color(effect.getColor().getRGB()));
-            emoji = effect.getActiveIcon();
-        } else if (CooldownManager.isOnCooldown(uuid, key)) {
-            long timeLeft = CooldownManager.getCooldownTimeLeft(uuid, key) / 1000L;
-            time = MessageUtil.formatTime(timeLeft, NamedTextColor.WHITE);
-        }
-
-        // Building the result
-        if (time.equals(Component.empty())) return Component.text(emoji);
-
-        return time.append(Message.toComponent("  <white>" + emoji).color(NamedTextColor.WHITE));
+    public String getSpaceTimeStr(String timeStr) {
+        return "\ue905".repeat(timeStr.length() - 1) + (timeStr.contains(":") ? "\ue904" : "\ue905");
     }
 
-    private Component getPart2(UUID uuid, String slot) {
-        Component time = Component.empty();
+    public void run2() {
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            UUID uuid = player.getUniqueId();
 
-        // Getting the effect equipped in the slot
-        EffectMapping effect = plugin.getDataManager().getEffect(uuid, slot);
+            // Composing the action bar
+            String key;
+            EffectMapping effect;
 
-        // Handling empty slots
-        boolean emptyEffectEmoji = plugin.getMainConfig().emptyEffectIcon();
-        if (effect == null) {
-            return emptyEffectEmoji ? Component.text("\uE901 ") : Component.empty();
-        }
+            String placeholder = plugin.getMainConfig().emptyEffectIcon() ? "\uE901" : "";
 
-        // Getting the right emoji to use and time to display
-        String key = effect.regular().getKey();
-        char emoji = effect.getIcon();
-        if (CooldownManager.isEffectActive(uuid, key)) {
-            long timeLeft = CooldownManager.getEffectTimeLeft(uuid, key) / 1000L;
-            time = MessageUtil.formatTime(timeLeft, TextColor.color(effect.getColor().getRGB()));
-            emoji = effect.getActiveIcon();
-        } else if (CooldownManager.isOnCooldown(uuid, key)) {
-            long timeLeft = CooldownManager.getCooldownTimeLeft(uuid, key) / 1000L;
-            time = MessageUtil.formatTime(timeLeft, NamedTextColor.WHITE);
-        }
+            String lSide = "";
+            // Loading info for the first effect
+            effect = plugin.getDataManager().getEffect(uuid, "1");
+            if (effect == null) {
+                lSide = " " + placeholder + "\ue904";
+            } else {
+                key = effect.regular().getKey();
+                if (CooldownManager.isEffectActive(uuid, key)) {
+                    long timeLeft = CooldownManager.getEffectTimeLeft(uuid, key) / 1000L;
+                    
+                    lSide = "<b><#" + Integer.toHexString(effect.getColor().getRGB() & 0xFFFFFF) + ">" + MessageUtil.formatTime(timeLeft) + "</b><white> \ue905" + effect.getActiveIcon();
+                } else if (CooldownManager.isOnCooldown(uuid, key)) {
+                    long timeLeft = CooldownManager.getCooldownTimeLeft(uuid, key) / 1000L;
+                    lSide = "<b><white>" + MessageUtil.formatTime(timeLeft) + "</b> " + effect.getIcon() + "\ue904";
+                } else {
+                    lSide = " " + effect.getIcon() + "\ue904";
+                }
+            }
 
-        // Building the result
-        if (time.equals(Component.empty())) return Component.text(emoji);
-        return Message.toComponent("<white>" + emoji + "  ").color(NamedTextColor.WHITE).append(time).color(NamedTextColor.WHITE);
+            // Loading info for the second effect
+            String rSide;
+            effect = plugin.getDataManager().getEffect(uuid, "2");
+            if (effect == null) {
+                rSide = "<white>" + placeholder + "\ue904 ";
+            } else {
+                key = effect.regular().getKey();
+                if (CooldownManager.isEffectActive(uuid, key)) {
+                    long timeLeft = CooldownManager.getEffectTimeLeft(uuid, key) / 1000L;
+                    rSide = "<white>" + effect.getActiveIcon() + "\ue905 <#" + Integer.toHexString(effect.getColor().getRGB() & 0xFFFFFF) + "><b>" + MessageUtil.formatTime(timeLeft) + "</b>";
+                } else if (CooldownManager.isOnCooldown(uuid, key)) {
+                    long timeLeft = CooldownManager.getCooldownTimeLeft(uuid, key) / 1000L;
+                    rSide = "<white>" + effect.getIcon() + "\ue904 <b>" + MessageUtil.formatTime(timeLeft) + "</b>";
+                } else {
+                    rSide = "<white>" + effect.getIcon() + "\ue904 ";
+                }
+            }
+
+            // Making sure both sides are the same length
+            int lSize = mm.stripTags(lSide).length();
+            int rSize = mm.stripTags(rSide).length();
+            Component msg = mm.deserialize(lSide + " " + rSide);
+
+            int diff = lSize - rSize;
+            Component spacing = Component.text("\ue904\ue904\ue904\ue904".repeat(Math.abs(diff)));
+
+            
+            if (diff > 0) {
+                msg = msg.append(spacing);
+            } else {
+                msg = spacing.append(msg);
+            }
+
+            // Sending the action bar
+            player.sendActionBar(msg);
+        });
     }
 }
