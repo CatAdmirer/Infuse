0df74d226b4ee12595ce55e4c1ae15002fa152cf
TurboJax <turbojax07@gmail.com>
2026-04-11 18:32:24 -0400

    FIXING EMERALD ENCHANTMENTS LFG

--- src/main/java/com/catadmirer/infuseSMP/effects/Emerald.java
+++ src/main/java/com/catadmirer/infuseSMP/effects/Emerald.java
@@ -9,16 +9,21 @@ import com.catadmirer.infuseSMP.managers.CooldownManager;
 import com.catadmirer.infuseSMP.managers.EffectMapping;
 import com.catadmirer.infuseSMP.util.ItemUtil;
 import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
+import com.google.common.collect.Lists;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.Enchantable;
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.keys.tags.EnchantmentTagKeys;
+import io.papermc.paper.registry.set.RegistryKeySet;
 import io.papermc.paper.registry.tag.Tag;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Stream;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -39,6 +44,7 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ItemType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -149,58 +155,142 @@ public class Emerald implements Listener {
         orb.setExperience(newAmount);
     }
 
+    public static record EnchantInstance(Enchantment enchantment, int level) {}
+
+    public static int getEnchantmentCost(Random random, int slot, ItemStack item) {
+        Enchantable enchantable = item.getData(DataComponentTypes.ENCHANTABLE);
+        if (enchantable == null) return 0;
+
+        int i = random.nextInt(8) + 8 + random.nextInt(16);
+        
+        if (slot == 0) return Math.max(i / 3, 1);
+
+        return slot == 1 ? i * 2 / 3 + 1 : Math.max(i, 30);
+    }
+
+    private List<EnchantInstance> getEnchantmentList(Player player, ItemStack item, int index, int baseCost, Random random) {
+        random.setSeed(player.getEnchantmentSeed() + index);
+        Registry<Enchantment> registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT);
+        Tag<Enchantment> allEnchantments = registry.getTag(EnchantmentTagKeys.IN_ENCHANTING_TABLE);
+        if (allEnchantments.isEmpty()) return List.of();
+
+        List<EnchantInstance> enchants = selectEnchantment(random, item, baseCost, allEnchantments.resolve(registry).stream());
+        if (item.getType() == Material.BOOK && enchants.size() > 1) {
+            enchants.remove(random.nextInt(enchants.size()));
+        }
+
+        return enchants;
+    }
+
+    public static List<EnchantInstance> selectEnchantment(Random random, ItemStack item, int cost, Stream<Enchantment> allEnchantments) {
+
+        Enchantable enchantable = item.getData(DataComponentTypes.ENCHANTABLE);
+        if (enchantable == null) return List.of();
+
+        cost += 1 + random.nextInt(enchantable.value() / 4 + 1) + random.nextInt(enchantable.value() / 4 + 1);
+        float f = (random.nextFloat() + random.nextFloat() - 1.0F) * 0.15F;
+        cost = Math.clamp(Math.round(cost + cost * f), 1, Integer.MAX_VALUE);
+        List<EnchantInstance> availableEnchants = getAvailableEnchantmentResults(cost, item, allEnchantments);
+        if (availableEnchants.isEmpty()) return List.of();
+
+        List<EnchantInstance> selectedEnchantments = new ArrayList<>();
+        selectedEnchantments.add(WeightedRandom.getRandomItem(random, availableEnchants, e -> e.enchantment.getWeight()));
+
+        while (random.nextInt(50) <= cost) {
+            // Filtering incompatible enchantments
+            if (!selectedEnchantments.isEmpty()) {
+                filterCompatibleEnchantments(availableEnchants, selectedEnchantments.getLast());
+            }
+
+            if (availableEnchants.isEmpty()) break;
+
+            selectedEnchantments.add(WeightedRandom.getRandomItem(random, availableEnchants, e -> e.enchantment.getWeight()));
+            cost /= 2;
+        }
+
+        return selectedEnchantments;
+    }
+
+    public static void filterCompatibleEnchantments(List<EnchantInstance> enchantments, EnchantInstance enchant) {
+        enchantments.removeIf(e -> enchant.enchantment().conflictsWith(e.enchantment()));
+    }
+
+    public static List<EnchantInstance> getAvailableEnchantmentResults(int cost, ItemStack item, Stream<Enchantment> allEnchantments) {
+        List<EnchantInstance> list = Lists.newArrayList();
+        boolean flag = item.getType() == Material.BOOK;
+        allEnchantments.filter(ench -> {
+            if (flag) return true;
+
+            RegistryKeySet<ItemType> primaryItems = ench.getPrimaryItems();
+            if (primaryItems == null) {
+                primaryItems = ench.getSupportedItems();
+            }
+
+            return primaryItems.contains(TypedKey.create(RegistryKey.ITEM, item.getType().key()));
+        }).forEach(e -> {
+            for (int i = e.getMaxLevel(); i >= 1; i--) {
+                if (cost >= e.getMinModifiedCost(i) && cost <= e.getMaxModifiedCost(i)) {
+                    list.add(new EnchantInstance(e, i));
+                    break;
+                }
+            }
+        });
+        return list;
+    }
+
     @EventHandler
     public void emeraldEnchantBonus(PrepareItemEnchantEvent event) {
-        if (!plugin.getDataManager().hasEffect(event.getEnchanter(), EffectMapping.EMERALD)) return;
-
-        // Getting the world seed of the player
-        long worldSeed = event.getEnchanter().getWorld().getSeed();
-
-        Random rand = new Random();
-        EnchantmentOffer[] currentOffers = event.getOffers();
-        Registry<Enchantment> enchantRegistry = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT);
-        Tag<Enchantment> inEnchantingTable = enchantRegistry.getTag(EnchantmentTagKeys.IN_ENCHANTING_TABLE);
-
         ItemStack item = event.getItem();
 
-        // Getting the enchantability of the item
-        Enchantable enchantable = item.getType().getDefaultData(DataComponentTypes.ENCHANTABLE);
-        if (enchantable == null) return;
+        // Skipping non-enchantable items
+        if (!item.hasData(DataComponentTypes.ENCHANTABLE)) return;
 
-        for (int i = 0; i < 3; i++) {
-            // Ofsetting the rng seed
-            rand.setSeed(worldSeed + i);
+        // Skipping already enchanted items
+        if (!item.getEnchantments().isEmpty()) return;
 
-            // Calculating the initial cost of the enchantment with 15 bookshelves
-            // The algorithm changes based on i
-            int cost = 0;
-            if (i == 0) cost = Math.max((rand.nextInt(8) + 8 + rand.nextInt(16)) / 3, 1);
-            if (i == 1) cost = (rand.nextInt(8) + 8 + rand.nextInt(16)) * 2 / 3 + 1;
-            if (i == 2) cost = Math.max(rand.nextInt(8) + 8 + rand.nextInt(16), 30);
+        // Making sure the enchanter has the emerald effect
+        Player player = event.getEnchanter();
+        if (!plugin.getDataManager().hasEffect(player, EffectMapping.EMERALD)) return;
 
-            // Calculating the final cost of the enchantment
-            cost += 1 + rand.nextInt(enchantable.value() / 4 + 1) + rand.nextInt(enchantable.value() / 4 + 1);
-            float f = (rand.nextFloat() + rand.nextFloat() - 1) * 0.15F;
-            cost = Math.clamp(Math.round(cost + cost * f), 1, Integer.MAX_VALUE);
-            final int finalCost = cost;
+        // mojang impl:
+        EnchantmentOffer[] offers = event.getOffers();
 
-            // Overriding the existing enchantment offers
-            if (!inEnchantingTable.isEmpty()) {
-                List<EnchantmentOffer> applicableEnchants = inEnchantingTable.resolve(enchantRegistry).stream()
-                        .filter(e -> e.canEnchantItem(item) || item.getType() == Material.BOOK)
-                        .map(e -> {
-                            for (int level = e.getMaxLevel(); level >= e.getStartLevel(); level--) {
-                                if (finalCost >= e.getMinModifiedCost(level) && finalCost <= e.getMaxModifiedCost(level)) {
-                                    return new EnchantmentOffer(e, level, finalCost);
-                                }
-                            }
-                            return null;
-                        }).filter(Objects::nonNull).toList();
+        Random random = new Random(player.getEnchantmentSeed());
 
-                // Overriding the current offer with a random one.
-                currentOffers[i] = WeightedRandom.getRandomItem(rand, applicableEnchants, e -> e.getEnchantment().getWeight());
+        // Calculating the costs
+        for (int k = 0; k < 3; k++) {
+            int cost = -1;
+
+            Enchantable enchantable = item.getData(DataComponentTypes.ENCHANTABLE);
+            if (enchantable == null) {
+                offers[k] = null;
+                continue;
+            }
+
+            int i = random.nextInt(1, 9) + 7 + random.nextInt(0, 16);
+            
+            if (k == 0) {
+                cost = Math.max(i / 3, 1);
+            } else if (k == 1) {
+                cost = i * 2 / 3 + 1;
+            } else {
+                cost = Math.max(i, 30);
+            }
+
+            if (cost < k + 1) {
+                offers[k] = null;
+                continue;
+            }
+
+            List<EnchantInstance> list = getEnchantmentList(player, item, k, cost, random);
+            if (!list.isEmpty()) {
+                EnchantInstance enchantmentinstance = list.get(random.nextInt(list.size()));
+                offers[k] = new EnchantmentOffer(enchantmentinstance.enchantment(), enchantmentinstance.level(), cost);
             }
         }
+
+        // On enchant:
+        // getEnchantmentList(player, item, i, costs[i], random);
     }
 
     @EventHandler
