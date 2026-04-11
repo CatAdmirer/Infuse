f1d88e8210b4e9f0aa4ec095fe2cfaa5a8e05e5b
TurboJax <turbojax07@gmail.com>
2026-04-11 19:33:08 -0400

    Redoing emerald enchanting with NMS

--- src/main/java/com/catadmirer/infuseSMP/effects/Emerald.java
+++ src/main/java/com/catadmirer/infuseSMP/effects/Emerald.java
@@ -2,35 +2,32 @@ package com.catadmirer.infuseSMP.effects;
 
 import com.catadmirer.infuseSMP.Infuse;
 import com.catadmirer.infuseSMP.InfuseDebug;
-import com.catadmirer.infuseSMP.WeightedRandom;
 import com.catadmirer.infuseSMP.events.EffectUnequipEvent;
 import com.catadmirer.infuseSMP.events.TenHitEvent;
 import com.catadmirer.infuseSMP.managers.CooldownManager;
 import com.catadmirer.infuseSMP.managers.EffectMapping;
 import com.catadmirer.infuseSMP.util.ItemUtil;
 import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
-import com.google.common.collect.Lists;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.Enchantable;
-import io.papermc.paper.registry.RegistryAccess;
-import io.papermc.paper.registry.RegistryKey;
-import io.papermc.paper.registry.TypedKey;
-import io.papermc.paper.registry.keys.tags.EnchantmentTagKeys;
-import io.papermc.paper.registry.set.RegistryKeySet;
-import io.papermc.paper.registry.tag.Tag;
-
-import java.util.ArrayList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.world.inventory.EnchantmentMenu;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import java.util.stream.Stream;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Particle;
-import org.bukkit.Registry;
 import org.bukkit.Sound;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.enchantments.CraftEnchantment;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentOffer;
 import org.bukkit.entity.ExperienceOrb;
@@ -42,9 +39,11 @@ import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerExpChangeEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ItemType;
+import org.bukkit.inventory.view.EnchantmentView;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -155,89 +154,6 @@ public class Emerald implements Listener {
         orb.setExperience(newAmount);
     }
 
-    public static record EnchantInstance(Enchantment enchantment, int level) {}
-
-    public static int getEnchantmentCost(Random random, int slot, ItemStack item) {
-        Enchantable enchantable = item.getData(DataComponentTypes.ENCHANTABLE);
-        if (enchantable == null) return 0;
-
-        int i = random.nextInt(8) + 8 + random.nextInt(16);
-        
-        if (slot == 0) return Math.max(i / 3, 1);
-
-        return slot == 1 ? i * 2 / 3 + 1 : Math.max(i, 30);
-    }
-
-    private List<EnchantInstance> getEnchantmentList(Player player, ItemStack item, int index, int baseCost, Random random) {
-        random.setSeed(player.getEnchantmentSeed() + index);
-        Registry<Enchantment> registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT);
-        Tag<Enchantment> allEnchantments = registry.getTag(EnchantmentTagKeys.IN_ENCHANTING_TABLE);
-        if (allEnchantments.isEmpty()) return List.of();
-
-        List<EnchantInstance> enchants = selectEnchantment(random, item, baseCost, allEnchantments.resolve(registry).stream());
-        if (item.getType() == Material.BOOK && enchants.size() > 1) {
-            enchants.remove(random.nextInt(enchants.size()));
-        }
-
-        return enchants;
-    }
-
-    public static List<EnchantInstance> selectEnchantment(Random random, ItemStack item, int cost, Stream<Enchantment> allEnchantments) {
-
-        Enchantable enchantable = item.getData(DataComponentTypes.ENCHANTABLE);
-        if (enchantable == null) return List.of();
-
-        cost += 1 + random.nextInt(enchantable.value() / 4 + 1) + random.nextInt(enchantable.value() / 4 + 1);
-        float f = (random.nextFloat() + random.nextFloat() - 1.0F) * 0.15F;
-        cost = Math.clamp(Math.round(cost + cost * f), 1, Integer.MAX_VALUE);
-        List<EnchantInstance> availableEnchants = getAvailableEnchantmentResults(cost, item, allEnchantments);
-        if (availableEnchants.isEmpty()) return List.of();
-
-        List<EnchantInstance> selectedEnchantments = new ArrayList<>();
-        selectedEnchantments.add(WeightedRandom.getRandomItem(random, availableEnchants, e -> e.enchantment.getWeight()));
-
-        while (random.nextInt(50) <= cost) {
-            // Filtering incompatible enchantments
-            if (!selectedEnchantments.isEmpty()) {
-                filterCompatibleEnchantments(availableEnchants, selectedEnchantments.getLast());
-            }
-
-            if (availableEnchants.isEmpty()) break;
-
-            selectedEnchantments.add(WeightedRandom.getRandomItem(random, availableEnchants, e -> e.enchantment.getWeight()));
-            cost /= 2;
-        }
-
-        return selectedEnchantments;
-    }
-
-    public static void filterCompatibleEnchantments(List<EnchantInstance> enchantments, EnchantInstance enchant) {
-        enchantments.removeIf(e -> enchant.enchantment().conflictsWith(e.enchantment()));
-    }
-
-    public static List<EnchantInstance> getAvailableEnchantmentResults(int cost, ItemStack item, Stream<Enchantment> allEnchantments) {
-        List<EnchantInstance> list = Lists.newArrayList();
-        boolean flag = item.getType() == Material.BOOK;
-        allEnchantments.filter(ench -> {
-            if (flag) return true;
-
-            RegistryKeySet<ItemType> primaryItems = ench.getPrimaryItems();
-            if (primaryItems == null) {
-                primaryItems = ench.getSupportedItems();
-            }
-
-            return primaryItems.contains(TypedKey.create(RegistryKey.ITEM, item.getType().key()));
-        }).forEach(e -> {
-            for (int i = e.getMaxLevel(); i >= 1; i--) {
-                if (cost >= e.getMinModifiedCost(i) && cost <= e.getMaxModifiedCost(i)) {
-                    list.add(new EnchantInstance(e, i));
-                    break;
-                }
-            }
-        });
-        return list;
-    }
-
     @EventHandler
     public void emeraldEnchantBonus(PrepareItemEnchantEvent event) {
         ItemStack item = event.getItem();
@@ -282,11 +198,35 @@ public class Emerald implements Listener {
                 continue;
             }
 
-            List<EnchantInstance> list = getEnchantmentList(player, item, k, cost, random);
-            if (!list.isEmpty()) {
-                EnchantInstance enchantmentinstance = list.get(random.nextInt(list.size()));
-                offers[k] = new EnchantmentOffer(enchantmentinstance.enchantment(), enchantmentinstance.level(), cost);
-            }
+            // event.getEnchantBlock()
+            // EnchantingTableBlock
+            // Getting the NMS EnchantmentMenu from the bukkit EnchantmentView
+            EnchantmentView view = event.getView();
+            try {
+                Field menuField = view.getClass().getDeclaredField("container");
+                menuField.setAccessible(true);
+                EnchantmentMenu menu = (EnchantmentMenu) menuField.get(view);
+                menuField.setAccessible(false);
+
+                Method getEnchantmentList = menu.getClass().getDeclaredMethod("getEnchantmentList", RegistryAccess.class, net.minecraft.world.item.ItemStack.class, int.class, int.class);
+                getEnchantmentList.setAccessible(true);
+
+                List<?> list = (List<?>) getEnchantmentList.invoke(menu, ((CraftWorld) player.getWorld()).getHandle().registryAccess(), CraftItemStack.asNMSCopy(item), k, cost);
+                if (!list.isEmpty()) {
+                    EnchantmentInstance enchantmentinstance = (EnchantmentInstance) list.get(random.nextInt(list.size()));
+                    offers[k] = new EnchantmentOffer(CraftEnchantment.minecraftHolderToBukkit(enchantmentinstance.enchantment), enchantmentinstance.level, cost);
+                }
+                getEnchantmentList.setAccessible(false);
+            } catch (NoSuchFieldException e) {
+                InfuseDebug.log("Could not load the EnchantmentMenu.  Emerald enchantments will not be modified");
+            } catch (NoSuchMethodException e) {
+                InfuseDebug.log("Could not find the \"getEnchantmentList\" method in the EnchantmentMenu class");
+            } catch (SecurityException e) {
+                InfuseDebug.log("Could not access the \"container\" field.");
+            } catch (IllegalArgumentException ignored) {
+            } catch (IllegalAccessException ignored) {
+            } catch (ClassCastException ignored) {
+            } catch (InvocationTargetException ignored) {}
         }
 
         // On enchant:
