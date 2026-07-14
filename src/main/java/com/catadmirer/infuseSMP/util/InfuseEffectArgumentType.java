f4aa614e66eac9114082b266f96bdf1919fefef5
TurboJax <turbojax07@gmail.com>
2026-07-14 00:09:56 -0400

    Adding various custom brigadier arguments

---
+++ src/main/java/com/catadmirer/infuseSMP/util/InfuseEffectArgumentType.java
@@ -0,0 +1,38 @@
+package com.catadmirer.infuseSMP.util;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.catadmirer.infuseSMP.effects.InfuseEffect;
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+
+public class InfuseEffectArgumentType implements ArgumentType<InfuseEffect> {
+    private static final DynamicCommandExceptionType ERROR_NO_EFFECT_FOUND = new DynamicCommandExceptionType(s -> new LiteralMessage("\"" + s + "\" is not a registered effect key."));
+
+    @Override
+    public InfuseEffect parse(StringReader reader) throws CommandSyntaxException {
+        String key = reader.readUnquotedString();
+
+        InfuseEffect effect = InfuseEffect.fromString(key);
+
+        if (effect == null) throw ERROR_NO_EFFECT_FOUND.create(key);
+
+        return effect;
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        InfuseEffect.getRegisteredEffects().forEach((i, e) -> {
+            builder.suggest(e.getRegularVersion().getKey());
+            builder.suggest(e.getAugmentedVersion().getKey());
+        });
+
+        return builder.buildFuture();
+    }
+}
