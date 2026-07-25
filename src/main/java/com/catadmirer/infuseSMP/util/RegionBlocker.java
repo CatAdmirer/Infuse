811744a7b192069361782cf5e72a0451b03a5305
TurboJax <turbojax07@gmail.com>
2026-07-25 16:49:53 -0400

    Adding functions to implement to RegionBlocker

--- src/main/java/com/catadmirer/infuseSMP/util/RegionBlocker.java
+++ src/main/java/com/catadmirer/infuseSMP/util/RegionBlocker.java
@@ -1,5 +1,12 @@
 package com.catadmirer.infuseSMP.util;
 
+import com.catadmirer.infuseSMP.effects.InfuseEffect;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.Set;
+
 public abstract class RegionBlocker {
     private static RegionBlocker instance;
 
@@ -10,6 +17,19 @@ public abstract class RegionBlocker {
     public static RegionBlocker getInstance() {
         return instance;
     }
+
+    public abstract boolean canUseSpark(Player player);
+
+    public abstract boolean canBeTargetedBySpark(Entity entity);
+    public abstract boolean canBeTargetedBySpark(Player player);
+    
+    public abstract Set<InfuseEffect> getAllowedEffects(Entity entity);
+    public abstract Set<InfuseEffect> getAllowedEffects(Player player);
+    public abstract Set<InfuseEffect> getAllowedEffects(Location loc);
+    
+    public abstract boolean isEffectAllowed(Entity entity, InfuseEffect effect);
+    public abstract boolean isEffectAllowed(Player player, InfuseEffect effect);
+    public abstract boolean isEffectAllowed(Location loc, InfuseEffect effect);
 
     // TODO: let people check for worldguard plugin
     // TODO: Make worldguardimpl a child of this class
