76b2809564af5fd776520d2aa90f624fcd8c5762
TurboJax <turbojax07@gmail.com>
2026-07-28 16:08:39 -0400

    Moving the current project into a "paper" submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,78 +1,25 @@
-plugins {
-    `java-library`
-    `maven-publish`
-    alias(libs.plugins.paperweight)
-    alias(libs.plugins.run.paper)
-}
+subprojects {
+    apply(plugin = "java")
 
-group = "com.catadmirer"
+    group = "com.catadmirer"
+    version = "2.4.5"
 
-val javaVersion = (project.property("javaVersion") as String).toInt()
-val minecraftVersion: String by project
-
-repositories {
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    maven("https://maven.enginehub.org/repo/")
-
-    maven("https://repo.papermc.io/repository/maven-public/")
-}
-
-dependencies {
-    compileOnly(libs.placeholderapi)
-    compileOnly(libs.worldguard)
-    compileOnly(libs.guava)
-    compileOnly(libs.gson)
-
-    paperweight.paperDevBundle("${minecraftVersion}+")
-}
-
-tasks.runServer {
-    // Configure the Minecraft version for our task.
-    // This is the only required configuration besides applying the plugin.
-    // Your plugin's jar (or shadowJar if present) will be used automatically.
-    minecraftVersion(minecraftVersion)
-    jvmArgs("-Dlog4j.configurationFile=log4j2.xml")
-}
-
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
-    sourceCompatibility = JavaVersion.toVersion(javaVersion)
-    targetCompatibility = JavaVersion.toVersion(javaVersion)
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-    options.release.set(javaVersion)
-}
-
-tasks.processResources {
-    val props = mapOf("version" to version,
-        "mcVersion" to minecraftVersion)
-    filesMatching("plugin.yml") {
-        expand(props)
+    configure<JavaPluginExtension> {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     }
-}
 
-tasks.register("resetAndRun") {
-    delete("run/plugins/$rootProject.name")
-    finalizedBy("runServer")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = "infuse"
-        }
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release = 21
     }
+
+    tasks.withType<Jar>().configureEach {
+        from("../LICENSE.txt")
+
+        archiveFileName.set("InfuseSMP-${project.name}-${project.version}.jar")
+    }
+
     repositories {
-        maven {
-            name = "turbo-maven"
-            url = uri("https://maven.turbojax.org/releases/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
+        mavenCentral()
     }
 }
---
+++ paper/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    `maven-publish`
+    alias(libs.plugins.paperweight)
+    alias(libs.plugins.run.paper)
+}
+
+val javaVersion = (project.property("javaVersion") as String).toInt()
+val minecraftVersion: String by project
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://maven.enginehub.org/repo/")
+}
+
+dependencies {
+    compileOnly(libs.placeholderapi)
+    compileOnly(libs.worldguard)
+    compileOnly(libs.guava)
+    compileOnly(libs.gson)
+
+    paperweight.paperDevBundle("${minecraftVersion}+")
+}
+
+tasks.runServer {
+    minecraftVersion(minecraftVersion)
+    jvmArgs("-Dlog4j.configurationFile=log4j2.xml")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version,
+        "mcVersion" to minecraftVersion)
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "infuse"
+        }
+    }
+    repositories {
+        maven {
+            name = "turbo-maven"
+            url = uri("https://maven.turbojax.org/releases/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
