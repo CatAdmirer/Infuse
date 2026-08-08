fa348649820023a6e398f3702e4660b84f43d3aa
TurboJax <turbojax07@gmail.com>
2026-08-08 08:56:41 -0400

    Beginning to work on some lore for the border effect.

--- src/main/java/com/catadmirer/infuseSMP/extraeffects/Border.java
+++ src/main/java/com/catadmirer/infuseSMP/extraeffects/Border.java
@@ -2,4 +2,22 @@ package com.catadmirer.infuseSMP.extraeffects;
 
 public class Border {
     //TODO hopefully soon
+    /**
+     * Border Lore:
+     * <red><b>ᴘᴀꜱꜱɪᴠᴇ ᴇꜰꜰᴇᴄᴛꜱ:
+     * <red>🚫 <dark_gray>
+     * <red>🚫 <dark_gray>
+     * <red>🚫 <dark_gray>
+     * <red>🚫 <dark_gray>
+     * <red>🚫 <dark_gray>
+     * <dark_gray>
+     * <red><b>ꜱᴘᴀʀᴋ ᴇꜰꜰᴇᴄᴛꜱ:
+     * <red>🚫 <dark_gray>Make a 16x16 border around you, trapping your enemies.
+     * <red>🚫 <dark_gray>This area does not move when it is summoned.
+     * <red>🚫 <dark_gray>Teammates can enter/exit the border freely.
+     * <red>🚫 <dark_gray>
+     * <dark_gray>
+     * <dark_aqua>ᴅᴜʀᴀᴛɪᴏɴ: 30s
+     * <dark_aqua>ᴄᴏᴏʟᴅᴏᴡɴ: 30s
+     */
 }
