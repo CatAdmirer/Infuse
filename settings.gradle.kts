bdf059f66970edb58ca183741ba3e7972dd03e3f
TurboJax <turbojax07@gmail.com>
2026-08-11 10:50:05 -0400

    Updating settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,22 @@
+includeBuild("build-logic")
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
+dependencyResolutionManagement {
+  repositories {
+    mavenCentral()
+  }
+}
+
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "InfuseSMP"
+
+include("api", "impl")
